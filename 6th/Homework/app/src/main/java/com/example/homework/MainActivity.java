dd725937638428946ace782fecd4668c7c9e1a29
sohn1029 <sohn1029@gmail.com>
2020-05-07 21:45:58 +0900

    6th homework submit

---
+++ 6th/Homework/app/src/main/java/com/example/homework/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.github);
+        myWebView.loadUrl("https://sohn1029.github.io/");
+    }
+
+}
